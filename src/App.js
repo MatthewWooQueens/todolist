74264ed02d6790bff82847f35ccd96a98ac38245
Matthew Woo <20mw6@queensu.ca>
2022-12-31 02:08:13 -0500

    Fixed up removal of elements from the list in the TodoItem function.

--- src/App.js
+++ src/App.js
@@ -2,23 +2,33 @@ import './App.css';
 import stuff from './20220708_010828.jpg';
 import { useEffect, useState } from 'react';
 
-function TodoItem (item){
-  console.log("Made it");
-  return(
-    <div className = "container">
-      <p>{item.test}</p>
-      <button className = "remove">
-        X
-      </button>
-    </div>
-  );
-
-}
 
 const App = () => {
   const [search1,setSearch] = useState("");
   const [slist,setList] = useState([]);
   useEffect(() =>{})
+
+  function TodoItem (item){
+    return(
+      <div className = "container">
+        <p>{item.test}</p>
+        <button className = "remove"
+          onClick={function(){
+            //slist.splice(slist.indexOf(item.test),1);
+            //console.log(slist);
+            setList(slist.filter(task => task != item.test));
+          }}
+        >
+          X
+        </button>
+      </div>
+    );
+  }
+
+  //const addTask = () => {
+  //  setList([...slist, {}])
+  //}
+
   return (
   <div className = "App">
     <div className = "App-header">
@@ -30,9 +40,14 @@ const App = () => {
       {search1?.length > 0?(
         <button className = "taskButton"
           onClick = {function() {
-          slist.push(search1);
-          console.log(slist);
-          setSearch("");
+            if (!slist.includes(search1)){
+              slist.push(search1);
+              
+              console.log(slist);
+            }else{
+              console.log("already exists");
+            }
+            setSearch("");
           }}
         >
         Add
@@ -49,7 +64,9 @@ const App = () => {
       )}
     </div>
     <div>
-      {slist.map((item) => <TodoItem test={item}/>)}
+      {slist.map((item)=>
+        <TodoItem test={item}/>
+      )}
     </div>
   </div>
   );
